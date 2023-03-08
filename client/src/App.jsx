d7ba694ebfc8fed92b38d02cd4c6d85a50ee6f84
Hung Bui <hungbui7690@gmail.com>
2023-03-08 09:22:25 -0700

    Inline Styles & Global CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+/*
+  Inline Styles & Global CSS
+  > npm install styled-components
+
+
+  + with inline style, when we need to reuse > copy 
+    > later, if we want to change > need to go to many places to change
+  + with className > cannot use simple className because of className collision 
+    > when project gets bigger, it's hard for maintain
+
+  >> Solution: styled-components > Next Lesson
+
+ */
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // (***) inline style + className (check css file)
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={{ padding: '2rem', backgroundColor: 'grey' }}>
+      <h1 className='title'>Styled Components</h1>
+      <button className='btn'>Click Me</button>
     </div>
   )
 }
