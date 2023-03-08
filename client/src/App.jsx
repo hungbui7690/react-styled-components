7075fbc2d79d9b1b539a65828210afd2c9ee83aa
Hung Bui <hungbui7690@gmail.com>
2023-03-08 09:25:14 -0700

    First Styled Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
 /*
-  Inline Styles & Global CSS
-  > npm install styled-components
-
-
-  + with inline style, when we need to reuse > copy 
-    > later, if we want to change > need to go to many places to change
-  + with className > cannot use simple className because of className collision 
-    > when project gets bigger, it's hard for maintain
-
-  >> Solution: styled-components > Next Lesson
+  First Styled Component
 
  */
 
+// (1)
+import styled from 'styled-components'
+
+// (2) cut from the index.css
+const BasicTitle = styled.h1`
+  text-align: center;
+  margin-bottom: 1rem;
+`
+
 function App() {
-  // (***) inline style + className (check css file)
   return (
     <div style={{ padding: '2rem', backgroundColor: 'grey' }}>
-      <h1 className='title'>Styled Components</h1>
+      {/* (3) reuse easily */}
+      <BasicTitle>Styled Components</BasicTitle>
+      <BasicTitle>Styled Components</BasicTitle>
+
       <button className='btn'>Click Me</button>
     </div>
   )
