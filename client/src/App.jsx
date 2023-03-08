605fd0713b89cde9daed62c687f50ad5f2c76541
Hung Bui <hungbui7690@gmail.com>
2023-03-08 10:28:11 -0700

    Simple Props

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,18 @@
 /*
-  Separate Files 
-  - since these are react components > we can divide in multiple files easily
-
-  (1) create folder components/BasicTitle.js
-    > Buttons.js > later, we want to have many Buttons in this file
+  Simple Props
+  - since they are react components > we can pass props 
 
  */
 
-// (***)
 import BasicTitle from './components/BasicTitle'
 import { DefaultButton } from './components/Buttons'
 
 function App() {
   return (
     <div style={{ padding: '2rem', backgroundColor: 'grey' }}>
-      <BasicTitle>Styled Components</BasicTitle>
+      {/* (1) pass props */}
+      <BasicTitle special>Styled Components</BasicTitle>
+
       <DefaultButton>Click Me</DefaultButton>
     </div>
   )
--- client/src/components/BasicTitle.js
+++ client/src/components/BasicTitle.js
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 const BasicTitle = styled.h1`
   text-align: center;
   margin-bottom: 1rem;
+
+  /* (2) in styled component > it's a little bit different > since this is css, first we need to have the property */
+  color: ${(props) => props.special && 'darkorange'};
 `
 
 export default BasicTitle
